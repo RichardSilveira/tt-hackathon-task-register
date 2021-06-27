44a3a4936b50d2736d2a3d96a5c4d3832f4da928
Richard Silva <richard.silva@bairesdev.com>
2021-06-27 00:23:18 -0400

    Containerizing environment | Updating dependencies and its configurations

--- task-register/task.js
+++ task-register/task.js
@@ -2,7 +2,7 @@ export const registerTasks = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: `Go Serverless v2.0! ${(await message({time: 1, copy: 'Your function executed successfully!'}))}`,
+            message: `Go Serverless v2.0!`,
         }),
     };
 }
@@ -11,7 +11,7 @@ export const getTasks = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: `Go Serverless v2.0! ${(await message({time: 1, copy: 'Your function executed successfully!'}))}`,
+            message: `Go Serverless v2.0!`,
         }),
     };
 }
