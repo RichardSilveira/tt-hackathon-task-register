aa6b06f8ed5dd225a80e95777d7199b86026e7ff
Richard Silva <richard.silva@bairesdev.com>
2021-06-27 14:59:42 -0400

    Dummy Requests to register and get tasks by user working - hard coded while configuring the slack app

--- task-register/task.js
+++ task-register/task.js
@@ -1,13 +1,45 @@
-export const registerTasks = async (event, context) => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: 'Go Serverless v2.0!',
-  }),
-});
+const AWS = require('aws-sdk');
+const registerTaskSampleJson = require('./registerTaskSample.json');
 
+AWS.config.update({ region: process.env.REGION });
+
+const stage = process.env.STAGE;
+const region = process.env.REGION;
+
+function logMetadata() {
+  console.log('environment variables:', {
+    stage,
+    region,
+  });
+}
+// curl -X POST http://localhost:4000/dev/tasks -H "Content-Type: application/json" -d @registerTaskSample.json
+export const registerTasks = async (event, context) => {
+  logMetadata();
+  console.log('3'); // testing the hot reloading changes
+
+  const registerTaskReq = JSON.parse(event.body);
+  console.log(registerTaskReq);
+
+  const registerTaskModel = registerTaskSampleJson;
+  console.log(registerTaskModel);
+
+  // todo: configure dynamodbmapper: https://www.one-tab.com/page/kRWYhVKPRpKwreAJa3WRNw
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      data: registerTaskModel,
+    }),
+  };
+};
+
+// npm i -g json ->
+// curl http://localhost:4000/dev/tasks/005013b9-6eb0-4d7d-a524-c506186ec071 -H "Content-Type: application/json" | json
 export const getTasks = async (event, context) => ({
   statusCode: 200,
   body: JSON.stringify({
-    message: 'Go Serverless v2.0!',
+    data: [registerTaskSampleJson,
+      { ...registerTaskSampleJson, taskDescription: 'Code review', taskTime: 3 },
+      { ...registerTaskSampleJson, taskDescription: 'meetings', taskTime: 4 }],
   }),
 });
